fcd2961a6445354d47ee3ea47fef4020e9f684c8
Tim Wentzlau <tim@wentzlau.dk>
2018-08-14 22:03:28 +0100

    optimize frame speed

--- kervi/platforms/raspberry/camera_driver.py
+++ kervi/platforms/raspberry/camera_driver.py
@@ -3,13 +3,14 @@ import time
 from PIL import Image, ImageDraw
 from kervi.vision.camera import FrameCameraDeviceDriver
 import picamera
+from threading import Condition
 
 class _StreamingMPEGBuffer(object):
     def __init__(self, driver):
         self.frame = None
         self.driver = driver
         self.buffer = io.BytesIO()
-        #self.condition = Condition()
+        self.condition = Condition()
 
     def write(self, buf):
         if buf.startswith(b'\xff\xd8'):
@@ -26,26 +27,7 @@ class _StreamingMPEGBuffer(object):
 class CameraDriver(FrameCameraDeviceDriver):
     def __init__(self):
         FrameCameraDeviceDriver.__init__(self)
-
-    def capture_framesx(self):
-        with picamera.PiCamera() as camera:
-            print("start Raspberry Pi camera")
-            camera.resolution = (self.camera.width, self.camera.height)
-            camera.framerate =  self.camera.fps
-            camera.hflip = self.camera.flip_horizontal
-            camera.vflip = self.camera.flip_vertical
-            camera.shutter_speed = 0
-            time.sleep(2)
-            stream = io.BytesIO()
-            for foo in camera.capture_continuous(stream, format="jpeg", use_video_port=True):
-                stream.seek(0)
-                image = Image.open(stream)
-                self.frame_ready(image.copy())
-                stream.seek(0)
-                self.wait_next_frame()
-                if self.terminate:
-                    break
-    
+        self._buffer_type = "jpeg"
 
     def capture_frames(self):
         with picamera.PiCamera() as camera:
@@ -59,8 +41,8 @@ class CameraDriver(FrameCameraDeviceDriver):
             output = _StreamingMPEGBuffer(self)
             
             camera.start_recording(output, format='mjpeg')
-            #while not self.terminate:
-            #    pass
-            #camera.stop_recording()
+            while not self.terminate:
+                time.sleep(1)
+            camera.stop_recording()
             
 
